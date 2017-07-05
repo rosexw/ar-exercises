64b654ae64a10376274527c1d1ba1233762c659d
Rose Wan <rose.wan@me.com>
2017-07-04 23:04:50 -0400

    exercise 7: Validations for both models

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -12,11 +12,11 @@ puts "----------"
 # We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a store_id (integer) column. This is a column that identifies which store each employee belongs to. It points to the id (integer) column of their store.
 # Let's tell Active Record that these two tables are in fact related via the store_id column.
 # Add the following code directly inside the Store model (class): has_many :employees
-class Store
+class Store < ActiveRecord::Base
   has_many :employees
 end
 # Add the following code directly inside the Employee model (class): belongs_to :store
-class Employee
+class Employee < ActiveRecord::Base
   belongs_to :store
 end
 # Add some data into employees. Here's an example of one (note how it differs from how you create stores): @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,30 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# Exercise 7: Validations for both models
+# Add validations to two models to enforce the following business rules:
+# Employees must always have a first name present
+# Employees must always have a last name present
+# Employees have a hourly_rate that is a number (integer) between 40 and 200
+# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, :inclusion => { :in => 40..200}
+  validates :store, presence: true
+end
+
+class Stores < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0, only_integer: true }
+end
+# Ask the user for a store name (store it in a variable)
+puts "What's the store name?"
+newStore = gets.chomp
+
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+@newStore = Store.create(name: newStore, id: 7)
+# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+puts @newStore.errors.messages
