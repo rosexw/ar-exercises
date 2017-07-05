59e0bb8e31e02ed0e47b0edda008484e3d4ab3e0
Rose Wan <rose.wan@me.com>
2017-07-04 22:34:10 -0400

    exercise 6: One to many association relationship

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,8 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-burnaby = Store.create(id: 1, name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true);
-richmond = Store.create(id: 2, name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true);
-gastown = Store.create(id: 3, name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false);
+Store.create(id: 1, name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true);
+Store.create(id: 2, name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true);
+Store.create(id: 3, name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false);
 
 puts Store.count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a store_id (integer) column. This is a column that identifies which store each employee belongs to. It points to the id (integer) column of their store.
+# Let's tell Active Record that these two tables are in fact related via the store_id column.
+# Add the following code directly inside the Store model (class): has_many :employees
+class Store
+  has_many :employees
+end
+# Add the following code directly inside the Employee model (class): belongs_to :store
+class Employee
+  belongs_to :store
+end
+# Add some data into employees. Here's an example of one (note how it differs from how you create stores): @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "Smith", hourly_rate: 100)
+# Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the @store# instance variables that you defined in previous exercises. Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...).
+@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 120)
+@store2.employees.create(first_name: "Tester", last_name: "Test", hourly_rate: 50)
+@store2.employees.create(first_name: "Joe", last_name: "Fresh", hourly_rate: 150)
